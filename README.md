c16b44527245f8bdd4e8be6967954cea531a4b1f
Hailong Wang <wang.seadragon@gmail.com>
2020-03-31 12:38:48 -0400

    Generalized calculate_ratio

--- README.md
+++ README.md
@@ -28,9 +28,15 @@ python3 merge_files.py --source_file_1 [1st source file] --source_file_2 [2nd so
 
 python3 deduplicate_gene_name.py --source_file [source file] --output_file [output file]
 
-# Calculate the ratio of column 6 over column 2, then calculate ratio of these 2 calculated values
+# Calculate the ratio of columns, source_file_2 is optional,
+## if only source_file_1 exist, it will only calculate ration with this file. For example, you want to have
+1st, 2nd, 5th, 6th and 2nd/6th in output file, then
 
-python3 calculate_ratio.py --source_file_1 [1st source file] --source_file_2 [2nd source file] --output_file [output file]
+python3 calculate_ratio.py --source_file_1 [1st source file] --columns 0,1,4,5,2/5 --output_file [output file]
+
+## if there are 2 source files, and you want to have 1st, 3rd(1st source file)/3rd(2nd source file), 5th(1st source file)/5th(2nd source file)
+
+python3 calculate_ratio.py --source_file_1 [1st source file] --source_file_2 [2nd source file] --columns 0,2/2,4/4 --output_file [output file]
 
 # Intersect 2 files based on gene name.
 
--- calculat_ratio.py
+++ calculat_ratio.py
@@ -7,29 +7,59 @@ def read(filename):
     with open(filename, newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter='\t', quotechar='|')
         for row in reader:
-           result[row[0]] = (row[1], row[5], float(row[5])/float(row[1]))
+            result[row[0]] = row
     return result
 
-def calculateRatio(source_file_1, source_file_2, output_file):
+def calculateRatio(source_file_1, source_file_2, columns, output_file):
     data = read(source_file_1)
-    data2 = read(source_file_2)
+    if source_file_2:
+        data2 = read(source_file_2)
+        with open(output_file, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile, delimiter='\t', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            for key in data.keys():
+                if key in data2.keys():
+                    row = [key]
+                    for col in columns:
+                        if '/' in col:
+                            cols = col.split('/')
+                            if cols[0] and cols[1]:
+                                row.append(float(data[key][cols[0]]) / float(data2[key][cols[1]]))
+                            elif cols[0]:
+                                row.append(data[key][cols[0]])
 
-    with open(output_file, 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile, delimiter='\t',quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        i = 0
-        for key in data.keys():
-            if data2[key][2] == 0:
-                continue;
-            if key in data2.keys():
-                writer.writerow([key, data[key][0], data[key][1], data[key][2], data2[key][0], data2[key][1], data2[key][2], data[key][2]/data2[key][2]])
-            i = i + 1
+                            elif cols[1]:
+                                row.append(data2[key][cols[1]])
+
+                        else:
+                            row.append(data[key][int(col)])
+                    writer.writerow(row)
+    else:
+        with open(output_file, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile, delimiter='\t',quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            for key in data.keys():
+                row = [key]
+                for col in columns:
+                    if col.contains('/'):
+                        cols = col.split('/')
+                        if cols[0] and cols[1]:
+                            row.append(float(data[key][cols[0]])/float(data[key][cols[1]]))
+                        elif cols[0]:
+                            row.append(data[key][cols[0]])
+
+                        elif cols[1]:
+                            row.append(data[key][cols[1]])
+
+                    else:
+                        row.append(data[key][int(col)])
+                writer.writerow(row)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Calculate the ratio of column 6 over column 2. ")
     parser.add_argument('--source_file_1', help="A source file delimited by tab ", action='store', dest='source_file_1')
     parser.add_argument('--source_file_2', help="a second source file", action='store', dest='source_file_2')
+    parser.add_argument('--columns', help="pairs of columns", action='store', dest='columns')
     parser.add_argument('--output_file', help='The output file', action='store', dest='output_file')
 
     args = parser.parse_args()
 
-    calculateRatio(args.source_file_1, args.source_file_2, args.output_file)
+    calculateRatio(args.source_file_1, args.source_file_2, args.columns.split(','), args.output_file)
