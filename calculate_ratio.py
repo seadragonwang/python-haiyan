2995b7a7107d3e5e466b2a7e5116a81ea40515f6
Hailong Wang <wang.seadragon@gmail.com>
2020-03-31 13:09:02 -0400

    Fixed bugs

--- calculate_ratio.py
+++ calculate_ratio.py
@@ -18,39 +18,53 @@ def calculateRatio(source_file_1, source_file_2, columns, output_file):
             writer = csv.writer(csvfile, delimiter='\t', quotechar='|', quoting=csv.QUOTE_MINIMAL)
             for key in data.keys():
                 if key in data2.keys():
-                    row = [key]
+                    row = []
+                    skip = False
                     for col in columns:
                         if '/' in col:
                             cols = col.split('/')
                             if cols[0] and cols[1]:
-                                row.append(float(data[key][cols[0]]) / float(data2[key][cols[1]]))
+                                if float(data2[key][int(cols[1])]) == 0:
+                                    skip = True;
+                                    break;
+                                else:
+                                    row.append(float(data[key][int(cols[0])]) / float(data2[key][int(cols[1])]))
                             elif cols[0]:
-                                row.append(data[key][cols[0]])
+                                row.append(data[key][int(cols[0])])
 
                             elif cols[1]:
-                                row.append(data2[key][cols[1]])
+                                row.append(data2[key][int(cols[1])])
 
                         else:
                             row.append(data[key][int(col)])
+                    if skip:
+                        continue;
                     writer.writerow(row)
     else:
         with open(output_file, 'w', newline='') as csvfile:
             writer = csv.writer(csvfile, delimiter='\t',quotechar='|', quoting=csv.QUOTE_MINIMAL)
             for key in data.keys():
-                row = [key]
+                row = []
+                skip = False;
                 for col in columns:
-                    if col.contains('/'):
+                    if '/' in col:
                         cols = col.split('/')
                         if cols[0] and cols[1]:
-                            row.append(float(data[key][cols[0]])/float(data[key][cols[1]]))
+                            if float(data[key][int(cols[1])]) == 0:
+                                skip = True;
+                                break;
+                            else:
+                                row.append(float(data[key][int(cols[0])])/float(data[key][int(cols[1])]))
                         elif cols[0]:
-                            row.append(data[key][cols[0]])
+                            row.append(data[key][int(cols[0])])
 
                         elif cols[1]:
-                            row.append(data[key][cols[1]])
+                            row.append(data[key][int(cols[1])])
 
                     else:
                         row.append(data[key][int(col)])
+                if skip:
+                    continue;
                 writer.writerow(row)
 
 if __name__ == '__main__':
